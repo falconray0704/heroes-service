c86c6e17fcb591d9a053f8e179c0ecdd1a7a0d79
FalconRay <falconray@yahoo.com>
2018-11-29 16:22:09 +0800

    Testing publish AIDE JSON DB to Fabric.

--- main.go
+++ main.go
@@ -3,21 +3,31 @@ package main
 import (
 	//"bufio"
 	"fmt"
-	"github.com/chainHero/heroes-service/blockchain"
 	"io"
 	"os"
+
+	"github.com/chainHero/heroes-service/blockchain"
+	pUtils "github.com/chainHero/heroes-service/plugins/utils"
+
 	//"log"
 	"os/exec"
 	"strconv"
+
 	//"syscall"
 	"time"
 )
+
+func aideLoadJSONDB(dbPath string) ([]byte, error) {
+	time.After(time.Second * 5)
+	return pUtils.GetBufFromFile(dbPath)
+}
 
 func targetScan() ([]byte, error) {
 	time.After(time.Second * 5)
 	fmt.Printf("+++ Reading info for publishing +++\n")
 	//cmd := exec.Command("aide", "-c ./aide.conf --check")
-	cmd := exec.Command("/bin/sh", "-c", "sudo aide -c ./aide.conf --check")
+	//cmd := exec.Command("/bin/sh", "-c", "sudo aide -c ./aide.conf --check")
+	cmd := exec.Command("/bin/sh", "-c", "sudo aide --check")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		//log.Fatal(err)
@@ -80,49 +90,50 @@ func main() {
 	}
 
 	/*
-	// Launch the web application listening
-	app := &controllers.Application{
-		Fabric: &fSetup,
-	}
-	web.Serve(app)
+		// Launch the web application listening
+		app := &controllers.Application{
+			Fabric: &fSetup,
+		}
+		web.Serve(app)
 	*/
 
 	////////////////////////////////
 
 	/*
-	//var pipeFile = "pipLogs"
-	var file *os.File
-	os.Remove(pipeFile)
-	e := syscall.Mkfifo(pipeFile, 0666)
-	if e != nil {
-		log.Fatal("Make named pipe file error:", e)
-	}
-	//go scheduleWrite()
-	fmt.Println("open a named pipe file for read.")
-	file, e := os.OpenFile(pipeFile, os.O_CREATE, os.ModeNamedPipe)
-	if e != nil {
-		log.Fatal("Open named pipe file error:", e)
-	}
+		//var pipeFile = "pipLogs"
+		var file *os.File
+		os.Remove(pipeFile)
+		e := syscall.Mkfifo(pipeFile, 0666)
+		if e != nil {
+			log.Fatal("Make named pipe file error:", e)
+		}
+		//go scheduleWrite()
+		fmt.Println("open a named pipe file for read.")
+		file, e := os.OpenFile(pipeFile, os.O_CREATE, os.ModeNamedPipe)
+		if e != nil {
+			log.Fatal("Open named pipe file error:", e)
+		}
 
-	reader := bufio.NewReader(file)
+		reader := bufio.NewReader(file)
 	*/
 	var line []byte
 
 	/*
-	for {
-		line, e := reader.ReadBytes('\n')
-		if e == nil {
-			fmt.Printf("load string:" + string(line))
-		} else {
-			//fmt.Printf("--- err:%s \n", err.Error())
-			time.Sleep(time.Millisecond * 100)
+		for {
+			line, e := reader.ReadBytes('\n')
+			if e == nil {
+				fmt.Printf("load string:" + string(line))
+			} else {
+				//fmt.Printf("--- err:%s \n", err.Error())
+				time.Sleep(time.Millisecond * 100)
+			}
 		}
-	}
 	*/
 
 	for {
 		//line, err = reader.ReadBytes('\n')
-		line, err = targetScan()
+		//line, err = targetScan()
+		line, err = aideLoadJSONDB("/usr/local/etc/aideDB/aide.db.new.json")
 		//fmt.Printf("+++ Got info:%s +++\n")
 		if err == nil {
 			key := strconv.FormatInt(time.Now().Unix(), 10)
@@ -142,7 +153,12 @@ func main() {
 			if err != nil {
 				fmt.Printf("--- Queried with key:%s fail! ---\n", key)
 			} else {
-				fmt.Printf("+++ Queried with key:%s get log:%s success! +++\n", key, v)
+				//fmt.Printf("+++ Queried with key:%s get log:%s success! +++\n", key, v)
+				if string(line) == v {
+					fmt.Printf("+++ Queried with key:%s success! +++\n", key)
+				} else {
+					fmt.Printf("--- Queried with key:%s success, but data incorrect! ---\n", key)
+				}
 			}
 		} else if err == io.EOF {
 			time.Sleep(time.Millisecond * 100)
